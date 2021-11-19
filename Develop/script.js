da9466453558c7260a4b2d234a2ea64455a8fdbf
bradleyjosephh <bradleyjosephh@gmail.com>
2021-11-18 19:53:31 -0800

    added local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 $(".btn").on("click",function(){
     var id = $(this).attr("id").split("-")[0]
     var plan = $("#"+ id +"-txt").val()
-    console.log(id,plan)
+    console.log(id, plan)
+    localStorage.setItem(id, plan)
 })
+
+for (let i = 8; i < 19; i++) {
+var chose = localStorage.getItem(i)
+$("#"+ i + "-txt").val(chose)
+}
