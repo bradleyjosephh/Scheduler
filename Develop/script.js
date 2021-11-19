abc69d5ce3920217cb259e8e7e2679211156e9ea
bradleyjosephh <bradleyjosephh@gmail.com>
2021-11-18 19:47:27 -0800

    added functionality to get time and whats written in text box

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 $(".btn").on("click",function(){
-    var id = $(this).attr("id")
-    console.log(id)
+    var id = $(this).attr("id").split("-")[0]
+    var plan = $("#"+ id +"-txt").val()
+    console.log(id,plan)
 })
